12d7e1a33549fad8f7c41e3286558f074f7e3a71
@armandoaepp - Armando Enrique Pisfil Puemape <amandoaepp@gmail.com>
2013-12-28 11:43:33 -0500

    archivo enlace contiene ejemplo de como trabajar con transacciones en poo

---
+++ model/Enlace.php
@@ -0,0 +1,54 @@
+<?php
+
+// require_once "../../capa_negocio/Funciones_Ajax/Fnc_Global.php";
+
+
+
+
+# =====================	Clases Php
+require_once "ClsConexion.php";
+require_once "ClsParametro.php";
+
+
+
+# =====================	Funciones Php
+// require_once "../../capa_negocio/Funciones_Ajax/Fnc_Comun.php";
+
+
+# INICIALIZAR XAJAX
+// $xajax->processRequest();
+
+try
+{
+
+
+	$objParametro = new ClsParametro() ;
+	# $nOriReg, $nCanReg, $nPagRegistro, $nParClase, $cParNombre="-", $cParDescripcion="-"
+	/*$data = $objParametro->Filtrar_Parametro(0 , 20 , 0 , 1001 , "-" , "-");
+
+		echo "Numero de Filas : ". count($data["cuerpo"])."<br>";
+
+		for($i = 0 ; $i < count( $data["cuerpo"]) ; $i++ ){
+		    echo  $data["cuerpo"][$i]["nParCodigo"]."-" . $data["cuerpo"][$i]["cParNombre"]." - ".$data["cuerpo"][$i]["cParDescripcion"]."</br>";
+		}*/
+	$objParametro->beginTransaction() ;
+		$data = $objParametro->Set_Parametro(1009, "NUEVO ", "NUEVA DESCRIPCION ");
+		$data = $objParametro->Set_Parametro(1011, "NUEVO ", "NUEVA DESCRIPCION ");
+		$data = $objParametro->Set_Parametro(1009, "NUEVO ", "NUEVA DESCRIPCION ");
+		$data = $objParametro->Set_Parametro(1009, "NUEVO ", "NUEVA DESCRIPCION ");
+		$data = $objParametro->Set_Parametro(1009, "NUEVO ", "NUEVA DESCRIPCION ");
+		$data = $objParametro->Set_Parametro(1009, "NUEVO ", "NUEVA DESCRIPCION ");
+		$data = $objParametro->Set_Parametro("nueoo", "NUEVO ", "NUEVA DESCRIPCION ");
+		$data = $objParametro->Set_Parametro(1009, "NUEVO ", "NUEVA DESCRIPCION ");
+	$objParametro->commit() ;
+
+		echo "insert  : ". count($data["cuerpo"])."<br>";
+		echo $data["cuerpo"][0]["nParCodigo"]."<br>" ;
+		echo $data["cuerpo"][0]["cParJerarquia"]."<br>" ;
+}catch(Exception $e ){
+	$objParametro->rollback() ;
+
+}
+
+
+?>
